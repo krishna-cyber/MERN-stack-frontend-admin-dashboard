f3a0eb7988de7f19b67d59a15d47370f869db6aa
Krishna Tiwari <61178259+krishna-cyber@users.noreply.github.com>
2024-12-21 10:37:53 +0545

    Remove unused React import in Users component

--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -1,6 +1,5 @@
 import { RightOutlined } from "@ant-design/icons";
 import { Breadcrumb } from "antd";
-import React from "react";
 import { NavLink } from "react-router-dom";
 
 const Users = () => {
