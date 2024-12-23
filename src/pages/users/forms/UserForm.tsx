aa33935bd73d40746e13f487266b47ce10860309
Krishna Tiwari <61178259+krishna-cyber@users.noreply.github.com>
2024-12-23 20:42:28 +0545

    Enhance UserForm component: add email and password fields, integrate tenant selection, and improve layout with Ant Design

--- src/pages/users/forms/UserForm.tsx
+++ src/pages/users/forms/UserForm.tsx
@@ -1,22 +1,94 @@
-import { Card, Col, Form, Input, Row } from "antd";
+import { useQuery } from "@tanstack/react-query";
+import { Card, Col, Form, Input, Row, Select, Space } from "antd";
+import { getTenants } from "../../../http/api";
 
 const UserForm = () => {
+  const { data: tenants } = useQuery({
+    queryKey: ["tenants"],
+    queryFn: async () => {
+      const res = await getTenants();
+      return res.data;
+    },
+  });
   return (
     <>
-      <Card title="Basic Information" size="small">
-        <Row gutter={20}>
-          <Col span={12}>
-            <Form.Item required label="First Name" name="firstName">
-              <Input />
-            </Form.Item>
-          </Col>
-          <Col span={12}>
-            <Form.Item required label="Last Name" name="lastName">
-              <Input />
-            </Form.Item>
-          </Col>
-        </Row>
-      </Card>
+      <Space direction="vertical" size={"middle"}>
+        <Card size="small" title="Basic Information" style={{ width: "100%" }}>
+          <Row gutter={20}>
+            <Col span={12}>
+              <Form.Item required label="First Name" name="firstName">
+                <Input size="large" allowClear />
+              </Form.Item>
+            </Col>
+            <Col span={12}>
+              <Form.Item required label="Last Name" name="lastName">
+                <Input size="large" allowClear />
+              </Form.Item>
+            </Col>
+            <Col span={12}>
+              <Form.Item required label="E-mail" name="email">
+                <Input size="large" allowClear />
+              </Form.Item>
+            </Col>
+          </Row>
+        </Card>
+
+        <Card
+          size="small"
+          title="Security Information"
+          style={{ width: "100%" }}
+        >
+          <Row gutter={20}>
+            <Col span={12}>
+              <Form.Item required label="Password" name="password">
+                <Input.Password size="large" allowClear />
+              </Form.Item>
+            </Col>
+            <Col span={12}>
+              <Form.Item required label="Confirm Password">
+                <Input.Password size="large" allowClear />
+              </Form.Item>
+            </Col>
+          </Row>
+        </Card>
+
+        <Card size="small" title="Role Information" style={{ width: "100%" }}>
+          <Row gutter={20}>
+            <Col span={12}>
+              <Form.Item required label="Roles" name="role">
+                <Select
+                  allowClear
+                  style={{ width: "100%" }}
+                  onChange={() => {}}
+                  placeholder="Roles"
+                >
+                  <Select.Option value={"admin"}>Admin</Select.Option>
+                  <Select.Option value={"manager"}>Manager</Select.Option>
+                  <Select.Option value={"customer"}>Customer</Select.Option>
+                </Select>
+              </Form.Item>
+            </Col>
+            <Col span={12}>
+              <Form.Item label="Resturants" name="tenantId">
+                <Select
+                  allowClear
+                  style={{ width: "100%" }}
+                  onChange={() => {}}
+                  placeholder="Resturants"
+                >
+                  {tenants?.result.map((tenant) => {
+                    return (
+                      <Select.Option value={tenant._id}>
+                        {tenant.name}
+                      </Select.Option>
+                    );
+                  })}
+                </Select>
+              </Form.Item>
+            </Col>
+          </Row>
+        </Card>
+      </Space>
     </>
   );
 };
